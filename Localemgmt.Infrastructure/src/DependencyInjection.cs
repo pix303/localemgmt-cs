6ba6aa6916f3dfb2025255d79c678d535c0514d5
paolo carraro <paolo.carraro@interlogica.it>
2024-09-26 14:59:44 +0200

    add configuration for event store (dynamoDB)

--- Localemgmt.Infrastructure/src/DependencyInjection.cs
+++ Localemgmt.Infrastructure/src/DependencyInjection.cs
@@ -1,6 +1,9 @@
-﻿using Localemgmt.Application.Users.Commons;
+﻿using EventSourcingStore;
+using Localemgmt.Application.Users.Commons;
 using Localemgmt.Infrastructure.Repositories.InMemory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
 
 namespace Localemgmt.Infrastructure;
 
@@ -9,6 +12,12 @@ public static class DependencyInjection
 	public static IServiceCollection AddInfrastructure(this IServiceCollection services)
 	{
 		services.AddSingleton<IUsersRepository, UsersRepository>();
+		services.AddScoped<IEventStore>(serviceProvider =>
+		{
+			var config = serviceProvider.GetRequiredService<IConfiguration>();
+			var tablename = config.GetSection("Store:TableName").Value;
+			return new DynamoDBEventStore(tablename ?? "no-table-name");
+		});
 		return services;
 	}
 }
