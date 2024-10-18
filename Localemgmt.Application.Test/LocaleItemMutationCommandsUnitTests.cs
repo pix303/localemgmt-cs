5436c86c3e6d70b8525fdb9d1c4a1dd7f88f6bc2
paolo carraro <paolo.carraro@interlogica.it>
2024-10-18 11:35:46 +0200

    update unit test for controller

--- Localemgmt.Application.Test/LocaleItemMutationCommandsUnitTests.cs
+++ Localemgmt.Application.Test/LocaleItemMutationCommandsUnitTests.cs
@@ -1,8 +1,5 @@
 using EventSourcingStore;
-using EventSourcingStore.Test;
-using Localemgmt.Application.LocaleItem.Commands.Add;
 using Localemgmt.Api.Config;
-using Localemgmt.Application.LocaleItem.Commands.Update;
 
 namespace Localemgmt.Application.Test;
 
@@ -17,7 +14,7 @@ public class StoreFixture
     }
 }
 
-public class LocaleItemMutationCommandsUnitTests : IClassFixture<StoreFixture>
+public class LocaleItemApplicationUnitTests : IClassFixture<StoreFixture>
 {
     string _lang = "it";
     string _context = "Default";
@@ -25,47 +22,8 @@ public class LocaleItemMutationCommandsUnitTests : IClassFixture<StoreFixture>
     string _userId = "123";
     StoreFixture _fixture;
 
-    public LocaleItemMutationCommandsUnitTests(StoreFixture fixture)
+    public LocaleItemApplicationUnitTests(StoreFixture fixture)
     {
         _fixture = fixture;
     }
-
-
-    [Fact]
-    public async void AddCommands_OkRequest()
-    {
-        var handler = new AddLocaleItemCommandHandler(_fixture.store);
-
-        var command = new AddLocaleItemCommand(
-            _lang,
-            _context,
-            _baseContent,
-            _userId
-        );
-
-        var result = await handler.Handle(command, CancellationToken.None);
-        Assert.NotEmpty(result.Value.AggregateId);
-        Assert.Equal(Localemgmt.Domain.LocaleItems.Events.LocaleItemEventTypes.LocaleItemAdded, result.Value.Type);
-    }
-
-    [Fact]
-    public async void UpdateCommands_OkRequest()
-    {
-        var handler = new UpdateLocaleItemCommandHandler(_fixture.store);
-        var id = Guid.NewGuid().ToString();
-        var command = new UpdateLocaleItemCommand(
-            _lang,
-            _context,
-            _baseContent,
-            _userId,
-            id
-        );
-
-        var result = await handler.Handle(command, CancellationToken.None);
-        Assert.Equal(id, result.Value.AggregateId);
-        Assert.Equal(Localemgmt.Domain.LocaleItems.Events.LocaleItemEventTypes.LocaleItemUpdated, result.Value.Type);
-    }
-
-
-
 }
