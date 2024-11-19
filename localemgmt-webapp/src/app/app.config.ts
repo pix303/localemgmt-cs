52db2c9d6b3bee016b910e7a01c0cdd233af90e1
paolo carraro <paolo.carraro@interlogica.it>
2024-11-19 18:15:09 +0100

    first commit

---
+++ localemgmt-webapp/src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideStore(reducers, { metaReducers })]
+};
